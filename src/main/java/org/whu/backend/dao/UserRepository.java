5936ac1f1202bfbe8c4d187bd762374cdb113d34
EnterprseBCMe <syh619833462@gmail.com>
2021-05-31 14:45:40 +0800

    重新创建项目以解决持久层bug

--- src/main/java/org/whu/backend/dao/UserRepository.java
+++ src/main/java/org/whu/backend/dao/UserRepository.java
@@ -2,12 +2,8 @@ package org.whu.backend.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-import org.springframework.stereotype.Repository;
 import org.whu.backend.pojo.User;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
-@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
